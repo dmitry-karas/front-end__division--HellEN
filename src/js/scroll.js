c3c9605dbde13031d329f0d9b69418da76efe905
Dmitry Karas <dmitrythekaras@gmail.com>
2021-01-31 17:26:21 +0200

    add footer anchor and scroll fix

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,9 +1,10 @@
  $(document).ready(function(){
-    $("#menu").on("click","a", function (event) {
+    $("#header").on("click","a", function (event) {
         event.preventDefault();
         var id  = $(this).attr('href'),
             top = $(id).offset().top;
         $('body,html').animate({ scrollTop: top }, 800);
+        
         
          $('.menu a').on('click', function () {
         $('.menu').removeClass('is-open');
